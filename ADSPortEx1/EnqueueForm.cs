1e6d0013f3a498bb074bbf1801996914b29c143e
Abdul Rafay Abdul Rafay <24853092@stu.mmu.ac.uk>
2025-01-09 22:53:59 +0500

    Task 1 A - GUI: Create form for inputting guest details into queue
    - Created EnqueueForm to input guest details (name and funds) for the queue.
    - Added textboxes for name and funds input.
    - Added button for enqueue action.

---
+++ ADSPortEx1/EnqueueForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADSPortEx1
+{
+    public partial class EnqueueForm : Form
+    {
+        public EnqueueForm()
+        {
+            InitializeComponent();
+        }
+
+        // funds input 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // name input 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Enqueue action 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
